ed7c2f31a7b1bc90188217ec37b877d3a46d342d
Jose Luis Rego Rodríguez <regorodrijl@gmail.com>
2017-03-15 16:37:41 +0100

    Modificaciones tweampues, añadido header y nav

--- Tweempus/src/app/app.module.ts
+++ Tweempus/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Tweempus/src/app/core/core.module.ts
+++ Tweempus/src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, NavComponent],
+  exports: [HeaderComponent, NavComponent]
 })
 export class CoreModule { }
